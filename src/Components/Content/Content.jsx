ebe92aadce7526fee95cfbf1ad8387fd863336fc
PaprikaStrv <78539418+PaprikaStrv@users.noreply.github.com>
2021-06-11 23:57:25 +0300

    footerAdd

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useState } from "react";
 import { Switch } from "react-router";
 import s from "./content.module.scss";
 import notific from "../../Images/Notifications.svg";
@@ -7,8 +7,20 @@ import avatar from "../../Images/Avatar.svg";
 import arrow from "../../Images/dropdown_icon.svg";
 import linza from "../../Images/linza.svg";
 import { ReactSVG } from "react-svg";
+import Cookies from "js-cookie";
+import { NavLink } from "react-router-dom";
 
 const Content = () => {
+  const [isLogoutActive, setLogoutActive] = useState(false);
+
+  const handlerAdminPanel = () => {
+    setLogoutActive(!isLogoutActive);
+  };
+
+  const handlerLogOut = () => {
+    Cookies.remove("userToken");
+    window.location.reload();
+  };
   return (
     <div className={s.contentWrapper}>
       <header>
@@ -17,25 +29,44 @@ const Content = () => {
             <ReactSVG src={linza} />
           </div>
 
-          <input type="text" placeholder="Поиск..." />
+          <input type="text" placeholder="Поиск ..." />
         </div>
         <div className={s.notificationWrapper}>
           <ReactSVG src={notific} />
-          <ReactSVG src={count} />
+          <div className={s.countNotification}>
+            <ReactSVG src={count} />
+          </div>
         </div>
-        <div className={s.adminBlockWrapper}>
-          <div>
+        <div
+          className={s.adminBlockWrapper}
+          onClick={() => handlerAdminPanel()}
+        >
+          {isLogoutActive && (
+            <div className={s.logOutWrapper}>
+              <button onClick={() => handlerLogOut()}>Выйти</button>
+            </div>
+          )}
+          <div className={s.adminAvatar}>
             <ReactSVG src={avatar} />
           </div>
 
-          <div>Админ</div>
+          <div className={s.adminName}>Admin</div>
           <div>
             <ReactSVG src={arrow} />
           </div>
         </div>
       </header>
       <Switch></Switch>
-      <footer></footer>
+      <footer>
+        <div className={s.footerContainer}>
+          <div>
+            <NavLink to="/">Главная страница</NavLink>
+            <NavLink to="/">Ссылка</NavLink>
+          </div>
+
+          <div className={s.coopyRightText}>Copyright © 2020 Simbirsoft</div>
+        </div>
+      </footer>
     </div>
   );
 };
